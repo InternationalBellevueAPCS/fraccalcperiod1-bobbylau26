55ffbd15d7ff367d2c0171da7ba30def4e1c72c7
s-laubo <s-laubo@49S50009947.it.bsd405.org>
2018-12-03 20:54:25 -0800

    Submitting Checkpoint 1

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,14 +1,21 @@
+/* Bobby Lau
+   APCS Period 1
+   December 3, 2018 */
+
+import java.util.*;
+
 public class FracCalc {
 
     /**
-     * Prompts user for input, passes that input to produceAnswer, then outputs the result.
-     * @param args - unused
+     * Prompts user to input an arithmetic expression, passes that input to produceAnswer() and then prints the second operand.
      */
-    public static void main(String[] args) 
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-        // Checkpoint 1: Create a Scanner, read one line of input, pass that input to produceAnswer, print the result.
+    public static void main(String[] args) {
         // Checkpoint 2: Accept user input multiple times.
+    	
+    	Scanner console = new Scanner(System.in);
+    	String expression = console.nextLine();
+    	String expressionAnswer = produceAnswer(expression);
+    	System.out.print(expressionAnswer);
     }
     
     /**
@@ -18,10 +25,18 @@ public class FracCalc {
      * @return the result of the fraction after it has been calculated.
      *      Example: return ==> "1_1/4"
      */
-    public static String produceAnswer(String input)
-    { 
-        // TODO: Implement this function to produce the solution to the input
-        // Checkpoint 1: Return the second operand.  Example "4/5 * 1_2/4" returns "1_2/4".
+    
+    public static String produceAnswer(String input) {
+        /* Break up line of input into the following strings:
+           1. First operand
+           2. Arithmetic operator (+ - * /)
+           3. Second operand
+        */
+    	String operand1 = input.substring(0, input.indexOf(" "));
+    	String restOfExpression = input.substring(input.indexOf(" ") + 1);
+    	String operator = restOfExpression.substring(0, restOfExpression.indexOf(" "));
+    	String operand2 = restOfExpression.substring(restOfExpression.indexOf(" ") + 1);
+    	
         // Checkpoint 2: Return the second operand as a string representing each part.
         //               Example "4/5 * 1_2/4" returns "whole:1 numerator:2 denominator:4".
         // Checkpoint 3: Evaluate the formula and return the result as a fraction.
@@ -29,8 +44,7 @@ public class FracCalc {
         //               Note: Answer does not need to be reduced, but it must be correct.
         // Final project: All answers must be reduced.
         //               Example "4/5 * 1_2/4" returns "1_1/5".
-        
-        return "";
+        return operand2;
     }
 
     // TODO: Fill in the space below with helper methods
@@ -43,8 +57,7 @@ public class FracCalc {
      * @param b - Second integer.
      * @return The GCD.
      */
-    public static int greatestCommonDivisor(int a, int b)
-    {
+    public static int greatestCommonDivisor(int a, int b) {
         a = Math.abs(a);
         b = Math.abs(b);
         int max = Math.max(a, b);
@@ -64,8 +77,7 @@ public class FracCalc {
      * @param b - Second integer.
      * @return The LCM.
      */
-    public static int leastCommonMultiple(int a, int b)
-    {
+    public static int leastCommonMultiple(int a, int b) {
         int gcd = greatestCommonDivisor(a, b);
         return (a*b)/gcd;
     }
